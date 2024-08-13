c828fff9e61371c1abe7eca147310076b99a5f42
Rogério Caetano dos Santos <rogerio.santos@docente.fieb.edu.br>
2024-08-13 20:43:56 -0300

    Model Categoria em construção

--- src/main/java/com/itb/mif3an/pizzaria/model/Categoria.java
+++ src/main/java/com/itb/mif3an/pizzaria/model/Categoria.java
@@ -1,7 +1,13 @@
 package com.itb.mif3an.pizzaria.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categorias")
 public class Categoria {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String descricao;
